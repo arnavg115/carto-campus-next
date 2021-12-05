d249ee495a42e70a26055a5185054e599a592e73
arnavg115 <soccercream20@gmail.com>
2021-12-05 22:59:36 +0000

    init

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+const Home: NextPage = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "grid",
+        placeItems: "center",
+      }}
+    >
+      <h1>Carto-Nextjs</h1>
+    </div>
+  );
+};
+
+export default Home;
