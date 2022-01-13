0ecaf6e001283be00213a3c3f43685eaf9d32a2d
Arnav G <arnavg@windowslinuxmac.com>
2022-01-12 19:04:22 -0800

    fixed ui

--- pages/500.tsx
+++ pages/500.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const FiveHundred = () => {
   return (
     <Box background="black" align="center" justify="center" height="100vh">
-      <h1 style={{ color: "red" }}>500</h1>
+      <h1 style={{ color: "red" }}>401</h1>
       <h3>Uh Oh! Looks like you are not Authorized for this page.</h3>
-      <Button label="Go Home" href="/" primary />
+      <Button label="Go Home" href="/" primary color="brand" />
     </Box>
   );
 };
